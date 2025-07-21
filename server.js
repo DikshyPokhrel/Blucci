01a7f5b0977632b15b157068c61c4d54d460a7c1
Dikshya <dikshya_pokharel@srmap.edu.in>
2025-07-21 08:24:17 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/livingroom-products', (req, res) => {
+  const folderPath = path.join(__dirname, 'public', 'Images', 'living-room');
+
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error('Error reading folder:', err);
+      return res.status(500).json({ error: 'Unable to scan directory' });
+    }
+
+    const images = files.filter(f => /\.(jpg|jpeg|png|gif)$/i.test(f));
+    res.json(images);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
